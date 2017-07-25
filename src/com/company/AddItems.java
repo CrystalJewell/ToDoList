74d3e5b6497c6569f52fabc6552a8b38d5e11ab3
Crystal Adkins <crystaladkins09@gmail.com>
2017-07-25 10:05:38 -0400

    Created method to allow for user input and add that input to a text file

--- src/com/company/AddItems.java
+++ src/com/company/AddItems.java
@@ -1,4 +1,26 @@
 package com.company;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class AddItems {
+
+
+    private void addTodoItems() {
+        //Prompt user for input
+        System.out.println("What would you like to add to your Todo list?");
+        //Create scanner to accept input
+        Scanner input = new Scanner(System.in);
+        //Create variable to hold user input
+        String todoItem = input.nextLine();
+
+        try {
+            Files.write(Paths.get("ToDoList.txt"), todoItem.getBytes());
+        } catch (IOException ioe) {
+            System.out.println("Unable to write to file");
+        }
+    }
+
 }
